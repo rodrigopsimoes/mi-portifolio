b03184c64fdec1f69674769d8776af2c3c53eb65
Rodrigo <rodrigo.simoes18@gmail.com>
2026-03-01 23:06:46 +0100

    feat: rediseño completo y carpeta docs actualizada

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/mi-portifolio/', // Ruta base correcta para GitHub Pages
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true, // Borra la carpeta antes de generar el nuevo build
+  },
 });
